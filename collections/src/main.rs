b858565e100f8030ac19f3ffd8f1cc648ccf0f09
eitaro.torii <eitaro.torii@exwzd.com>
2018-10-18 19:07:11 +0900

    8.2まで

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -5,6 +5,8 @@ enum SpreadsheetCell {
     Text(String),
 }
 
+use std::collections::HashMap;
+
 fn main() {
     let v: Vec<i32> = Vec::new();
     let v = vec![1.0, 2.0];
@@ -72,4 +74,56 @@ fn main() {
         println!("{:?}", i);
     }
 
+    let mut s = String::new();
+    let data = "🍣 initial contents α "; // 寿司
+    s = data.to_string();
+    println!("{}", s);
+
+    s.push_str("bar");
+    println!("{}", s);
+
+    let mut s = String::from("lo");
+    s.push('l');
+    println!("{}", s);
+
+    let s1 = String::from("Hello,");
+    let s2 = String::from("World");
+    let s3 = s1 + &s2; // add method
+    // println!("{}", s1); // moveされてるのでコンパイルできない
+    println!("{}", s2); // 借用されてるだけなので使える
+    println!("{}", s3);
+
+    /* // これも動くが、shadowingがうまく機能してない...
+    let mut s1 = String::from("Hello,");
+    let s2 = String::from("World");
+    s1 = s1 + &s2; // add method
+    println!("{}", s1); // moveされてるけど戻ってきたので動く
+    println!("{}", s2); // 借用されてるだけなので使える
+     */
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{}-{}-{}", s1, s2, s3); // 多分コピーしている
+    println!("{}", s);
+    println!("{}", s1); // 所有権を奪わない、便利
+
+    let x = String::from("こんにちわ");
+    println!("{}", x);
+    let y = &x[0..3]; // 嫌すぎるけどスライスが作れる
+    //let y = &x[0..2]; //文字の境界以外で区切ったらランタイムエラー
+    println!("{}", y);
+
+    for c in "どらえもん".chars() {
+        println!("{}", c); // 「ど」が出力される
+    }
+
+    for c in "どらえもん".chars() { // macのファイルシステムから持ってきた文字列
+        println!("{}", c); // 「と」「 ゙」が出力される
+    }
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("White"), 30);
+    println!("{:?}", scores);
 }
