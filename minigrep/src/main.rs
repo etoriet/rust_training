75e3ce6c1b4b1f6cf4c27fc1ae92fa2118c863f1
eitaro.torii <eitaro.torii@exwzd.com>
2019-03-01 19:06:36 +0900

    12章途中

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,9 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,42 +13,8 @@ fn main() {
         process::exit(1);
     });
 
-    let c = {
-        let v: Vec<String> = vec![String::from("a"), String::from("b"), String::from("c")];
-        Config::new(&v)
-    };
-    let c = c.unwrap_or_else(|err| {
-        println!("Problem while parsing args: {}", err);
-        process::exit(1)
-    });
-
-
-    println!("searching {}", config.query);
-    println!("in file {}", config.filename);
-
-
-    let mut f = File::open(config.filename).expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents).expect("something wrong");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> { // こっちの方が正しい(strが'staticのlifetime)はずだが、一旦どこでコンパイルエラーになるかをみる
-    //fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough argument");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
